ab99c56463b5ac12ffe17b41a6c9df081bda92b4
Victor Emanuel <victorgit28@outlook.com>
2025-10-21 19:00:11 -0300

    Change 2

--- server.js
+++ server.js
@@ -7,14 +7,11 @@ const mysql = require('mysql2/promise');
 
 const app = express();
 
-// / Configuração básica via variáveis de ambiente
 const PORT = process.env.PORT || 80;
-
 const DB_HOST = process.env.DB_HOST || 'up-de-fra1-mysql-1.db.run-on-seenode.com';
-
 const DB_USER = process.env.DB_USER || 'db_dtnidddiwulw';
-
 const DB_PASSWORD = process.env.DB_PASSWORD || 'cI8C9O2nSwZ2ZmHfgJW5phzi';
+const DB_NAME = process.env.DB_NAME || 'db_dtnidddiwulw';
 
 // Configuração básica via variáveis de ambiente
 // const PORT = process.env.PORT || 3000;
